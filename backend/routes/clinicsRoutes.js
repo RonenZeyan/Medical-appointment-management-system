7b6792fb281ee17ae162a7bcd9604beeac3de220
RonenZeyan <101258047+RonenZeyan@users.noreply.github.com>
2025-02-21 00:49:58 +0200

    delete style.css

--- backend/routes/clinicsRoutes.js
+++ backend/routes/clinicsRoutes.js
@@ -4,6 +4,7 @@ const { verifyTokenIsAdmin } = require("../middlewares/verifyToken");
 const validateObjectId = require("../middlewares/validateObjectId");
 const router = express.Router();
 
+
 router.get("/", getAllClinics);
 router.post("/new-clinic", verifyTokenIsAdmin, addNewClinic);
 router.delete("/:id", validateObjectId, verifyTokenIsAdmin, deleteClinic);

9d4de2c61577a832599ac9803b96432163fc0d71
yareenkezel <yareenkezel77@gmail.com>
2025-02-27 01:53:43 +0200

    yareen-brach files

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,12 @@
 //modules imports
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/authController");
+const { registerUser, loginUser ,AddUser} = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/AdminAddUser", AddUser);
+
 router.post("/login", loginUser);
+
 
 module.exports = router;

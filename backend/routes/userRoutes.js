e13a2911172654710fc0faf1a5f4d29353873ee9
RonenZeyan <101258047+RonenZeyan@users.noreply.github.com>
2025-03-01 17:02:06 +0200

    get data from github

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -23,15 +23,18 @@ const {
 // Get user by name route
 router.post("/findUser", verifyTokenIsAdmin, getUserByName);
 
-// Get user by id
-router.post("/userInfo/:id",verifyTokenIsSameUserOrAdmin, getUserInfo);
-
-// Get user by ID
-// router.get("/:id", verifyTokenIsAdmin, getUserById);
-
+//get doctors
+router.get("/doctors", getAllDoctors);
 
 // Get all users (only admin)
 router.get("/", verifyTokenIsAdmin, getAllUsers);
+
+// Get user by name
+router.post("/findDoctor",verifyToken, getDoctorByName);
+
+// Get user by id
+router.post("/userInfo/:id",verifyTokenIsSameUserOrAdmin, getUserInfo);
+
 
 // Delete user by ID (only admin)
 router.delete("/:id", verifyTokenIsAdmin, deleteUserById);
@@ -39,10 +42,7 @@ router.delete("/:id", verifyTokenIsAdmin, deleteUserById);
 // Update user info (only admin and user)
 router.put("/:id", verifyTokenIsSameUserOrAdmin, updateUserInfo);
 
-//get doctors
-router.get("/doctors", getAllDoctors);
-// Get user by name
-router.post("/findDoctor",verifyToken, getDoctorByName);
-
+// Get user by ID
+router.get("/:id", verifyTokenIsAdmin, getUserById);
 
 module.exports = router;

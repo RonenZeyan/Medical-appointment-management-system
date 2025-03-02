2fef768233635c52815b51881c1364bfcac30e1a
RonenZeyan <101258047+RonenZeyan@users.noreply.github.com>
2025-03-02 12:49:14 +0200

    fix conflicts

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -22,14 +22,6 @@ const {
 
 
 
-// Get user by name route
-router.post("/findUser", verifyTokenIsSameUserOrAdmin, getUserByName);
-
-// Get user by id
-router.post("/userInfo/:id", verifyTokenIsSameUserOrAdmin, getUserInfo);
-
-// Get user by ID
-router.get("/:id", verifyTokenIsAdmin, getUserById);
 //get doctors
 router.get("/doctors", getAllDoctors);
 
@@ -37,17 +29,25 @@ router.get("/doctors", getAllDoctors);
 router.get("/", verifyTokenIsAdmin, getAllUsers);
 
 
+// Add User
+router.post("/AdminAddUser", AddUser);
+
+// Get user by ID
+router.get("/:id", verifyTokenIsAdmin, getUserById);
+
 // Delete user by ID (only admin)
 router.delete("/:id", verifyTokenIsAdmin, deleteUserById);
 
 // Update user info (only admin and user)
 router.put("/:id", verifyTokenIsSameUserOrAdmin, updateUserInfo);
 
+// Get user by name route
+router.post("/findUser", verifyTokenIsAdmin, getUserByName);
+
+// Get user by id
+router.post("/userInfo/:id", verifyTokenIsSameUserOrAdmin, getUserInfo);
+
 // Get doctor by name
 router.post("/findDoctor", verifyToken, getDoctorsByParameters);
-
-// Add User
-router.post("/AdminAddUser", AddUser);
-
 
 module.exports = router;
